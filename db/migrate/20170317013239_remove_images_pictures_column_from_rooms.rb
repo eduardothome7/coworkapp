d1f30424bb1174c57b0069240ef273b52f5f4aa2
eduardo cannini thome <eduardocanninithome@hotmail.com>
2017-03-16 22:49:11 -0300

    fix migrations

--- db/migrate/20170317013239_remove_images_pictures_column_from_rooms.rb
+++
@@ -1,7 +0,0 @@
-# class RemoveImagesPicturesColumnFromRooms < ActiveRecord::Migration[5.0]
-#   def change
-#     remove_column :rooms, :images
-#     remove_column :rooms, :pictures
-#     remove_column :rooms, :picture
-#   end
-# end
